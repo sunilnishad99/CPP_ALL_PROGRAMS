e1ef87f9f150993068a68e96e16ed1f1772506d0
sunilnishad99 <151985661+sunilnishad99@users.noreply.github.com>
2024-05-21 00:53:21 +0530

    Add files via upload

---
+++ tut5.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int num1 , num2;
+   // cout<< it is useing to intertion operator(output ko screen per dedo)
+  // cin>> is called EXTRACTION operator(user se value lene k liye)
+   cout<< "Enter te value of num1 and num2:\n";
+   cin>> num1 >> num2;
+   cout<< "the value of num1 and num2 is=>"<<num1+num2;
+return 0;
+}
